13f4a505757c395165dfe58c2f54acf3c9105e6d
Jefferson Villa Cortés <jeffersonvillacortes@gmail.com>
2023-11-16 17:05:37 -0500

    Agrego cross origin a los endpoints

--- src/main/java/com/jeffersonvilla/apifestivos/controladores/FestivosControlador.java
+++ src/main/java/com/jeffersonvilla/apifestivos/controladores/FestivosControlador.java
@@ -3,6 +3,7 @@ package com.jeffersonvilla.apifestivos.controladores;
 import java.util.Calendar;
 import java.util.List;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -21,6 +22,7 @@ public class FestivosControlador {
         this.servicio = servicio;
     }
 
+    @CrossOrigin(origins = "http://localhost:4200/")
     @GetMapping("/verificar/{anio}/{mes}/{dia}")
     public String verficarFestivo(@PathVariable int anio, @PathVariable int mes, @PathVariable int dia){
         String fechaStr = Integer.valueOf(anio) + "-" + Integer.valueOf(mes) + "-" +Integer.valueOf(dia);
@@ -34,6 +36,7 @@ public class FestivosControlador {
         }
     }
 
+    @CrossOrigin(origins = "http://localhost:4200/")
     @GetMapping("/obtener/{año}")
     public List<FestivoDTO> listarFestivos(@PathVariable int año){
         return servicio.obtenerFestivos(año);
